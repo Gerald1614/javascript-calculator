269c5e7d01b0352f5c728852b76d64f08402844c
Gerald Michelant <gerald.michelant@gmail.com>
2017-11-16 16:21:39 -0500

    result line added

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,68 @@
 var operation = "";
+var opRes="";
 
 function afficherResultat(){
-  operation=eval(operation).toString();
-  if (operation.length >16) {
-    operation=operation.slice(0,-4);
+  if (operation !=""){
+  operation=eval(operation).toFixed(4).toString();
+  if (operation.length >17) {
+    valExcess();
+    operation=0;
+    setTimeout(function() {   
+      operation="";
+      opRes="";
+      afficherEcran(operation, opRes);
+   }, 4000);
   }
-  afficherEcran(operation);
+  afficherEcran(operation, opRes);
+ 
+}
 };
 
-
+function valExcess(){
+  $('#ecran').tooltip({title: "limit exceeded"});
+  $('#ecran').tooltip('show');
+  $('#ecran').on('shown.bs.tooltip', function () {
+    setTimeout(function() {   
+      $('#ecran').tooltip('dispose');
+   }, 4000);
+  })
+}
 function checkEntry(value) {
   if (operation.length<18){
      operation+=value;
+     opRes+=value;
   }
   else {
-    $('#ecran').tooltip({title: "limit exceeded"});
-    $('#ecran').tooltip('show');
-    $('#ecran').on('shown.bs.tooltip', function () {
-      setTimeout(function() {   
-        $('#ecran').tooltip('dispose');
-     }, 4000);
-    })
+    valExcess();
   }
 
   if (operation.length<2) {
    if (value =="+" || value =="*" || value=="/" || value=="=") {
         operation="";
+        opRes="";
     }
   }
    if(operation.length>1 && isNaN(parseInt(operation.charAt(operation.length-2))) && isNaN(parseInt(value)) ) {
      operation=operation.slice(0,-1);
+     opRes=opRes.slice(0,-1);
    }
-  afficherEcran(operation);
+  afficherEcran(operation, opRes);
 };
 
 function annulerDernier() {
-operation=operation.slice(0,-1)
-afficherEcran(operation);
+operation=operation.slice(0,-1);
+opRes=opRes.slice(0,-1);
+afficherEcran(operation, opRes);
 }
 
 function annulerTout(){
  operation="";
- afficherEcran(operation);
+ opRes="";
+ afficherEcran(operation, opRes);
 }
 
-function afficherEcran(operation){
-  document.getElementById("ecran").innerHTML=operation;
+function afficherEcran(operation, opRes){
+  document.getElementById("res").innerHTML=operation;
+  document.getElementById("opLine").innerHTML=opRes;
+
 }
