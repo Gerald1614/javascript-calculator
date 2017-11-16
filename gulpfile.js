e757e0217cbffa2261286726689fd46f8d9841c4
Gerald Michelant <gerald.michelant@gmail.com>
2017-11-15 22:40:11 -0500

    js calculator

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,8 @@ concat = require('gulp-concat'),
 deporder = require('gulp-deporder'),
 stripdebug = require('gulp-strip-debug'),
 uglify = require('gulp-uglify'),
-sass = require('gulp-sass'),
-postcss = require('gulp-postcss'),
-assets = require('postcss-assets'),
-autoprefixer = require('autoprefixer'),
-mqpacker = require('css-mqpacker'),
-cssnano = require('cssnano'),
+cleanCSS = require('gulp-clean-css');
+
 
 // development mode?
 devBuild = (process.env.NODE_ENV !== 'production'),
@@ -23,13 +19,13 @@ devBuild = (process.env.NODE_ENV !== 'production'),
 // folders
 folder = {
   src: 'src/',
-  build: 'build/'
+  dist: 'dist/'
 };
 
 gulp.task('browser-sync', function () {
   var files = [
      folder.src+'*.html',
-     folder.src+'scss/*.scss',
+     folder.src+'css/*.css',
      folder.src+'images/*.{png,jpg,gif, jpeg}',
      folder.src+'js/*.js'
   ];
@@ -44,7 +40,7 @@ gulp.task('browser-sync', function () {
 
 // image processing
 gulp.task('images', function() {
-    var out = folder.build + 'images/';
+    var out = folder.dist + 'images/';
     return gulp.src(folder.src + 'images/**/*')
       .pipe(newer(out))
       .pipe(imagemin({ optimizationLevel: 5 }))
@@ -53,11 +49,12 @@ gulp.task('images', function() {
 
   // HTML processing
 gulp.task('html', function() {
+ 
     var
-      out = folder.build ,
+      out = folder.dist,
       page = gulp.src(folder.src + '/*.html')
         .pipe(newer(out));
-  
+
     // minify production code
     if (!devBuild) {
       page = page.pipe(htmlclean());
@@ -79,49 +76,32 @@ gulp.task('js', function() {
           .pipe(uglify());
       }
     
-      return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
+      return jsbuild.pipe(gulp.dest(folder.dist + 'js/'));
     
     });
 
-    // CSS processing
-gulp.task('css', function() {
-    
-      var postCssOpts = [
-      assets({ loadPaths: ['images/'] }),
-      autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
-      mqpacker
-      ];
-    
-      if (!devBuild) {
-        postCssOpts.push(cssnano);
-      }
-    
-      return gulp.src(folder.src + 'scss/main.scss')
-        .pipe(sass({
-          outputStyle: 'nested',
-          imagePath: 'images/',
-          precision: 3,
-          errLogToConsole: true
-        }))
-        .pipe(postcss(postCssOpts))
-        .pipe(gulp.dest(folder.build + 'css/'));
-    
+    gulp.task('minify-css', () => {
+      return gulp.src(folder.src +'css/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest(folder.dist+ 'css'));
     });
+
+   
 
     // watch for changes
 gulp.task('watch', function() {
         
       // html changes
-      gulp.watch(folder.src + '*.html', gulp.series('html'));
+      gulp.watch( '*.html', gulp.series('html'));
     
       // javascript changes
-      gulp.watch(folder.src + 'js/**/*', gulp.series('js'));
+      gulp.watch( 'js/**/*.js', gulp.series('js'));
     
       // css changes
-      gulp.watch(folder.src + 'scss/**/*', gulp.series('css'));
+      gulp.watch( 'css/**/*.css', gulp.series('minify-css'));
     
     });
 
-    gulp.task('run', gulp.series('html', 'css', 'js', 'browser-sync'));
+    gulp.task('build', gulp.series('html','minify-css', 'js', 'images', 'browser-sync'));
 
-    gulp.task('default', gulp.series('run', 'watch'));
+    gulp.task('default', gulp.series('build', 'watch'));
---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+let operation = "";
+
+function afficherResultat(){
+  operation=eval(operation).toString();
+  if (operation.length >16) {
+    operation=operation.slice(0,-4);
+  }
+  afficherEcran(operation);
+};
+
+function checkEntry(value) {
+  if (operation.length<18){
+     operation+=value;
+  }
+  else {
+    $('#ecran').tooltip({title: "limit exceeded"});
+    $('#ecran').tooltip('show');
+    $('#ecran').on('shown.bs.tooltip', function () {
+      setTimeout(function() {   
+        $('#ecran').tooltip('dispose');
+     }, 4000);
+    })
+  }
+
+  if (operation.length<2) {
+   if (value =="+" || value =="*" || value=="/" || value=="=") {
+        operation="";
+    }
+  }
+   if(operation.length>1 && isNaN(parseInt(operation.charAt(operation.length-2))) && isNaN(parseInt(value)) ) {
+     operation=operation.slice(0,-1);
+   }
+  afficherEcran(operation);
+};
+
+function annulerDernier() {
+operation=operation.slice(0,-1)
+afficherEcran(operation);
+}
+
+function annulerTout(){
+ operation="";
+ afficherEcran(operation);
+}
+
+function afficherEcran(operation){
+  document.getElementById("ecran").innerHTML=operation;
+}
